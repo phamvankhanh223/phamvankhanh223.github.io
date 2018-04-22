5d4baa31504aaeafc06b02b0f0f6803f22af200c
khanh pham <pham van khanh@DESKTOP-1SRG42G>
2018-04-22 16:21:48 +0700

    them khối isotop

--- 1.js
+++ 1.js
@@ -1,4 +1,15 @@
 ﻿ $(function(){
+ 		
+ 	$hieuung=$('.noidung').isotope({
+ 		itemSelector:'.khoianh',
+ 		layoutMode:'masonry'
+
+ 	});
+
+ 	$hieuung.imagesLoaded().progress(function(){
+ 		$hieuung.isotope('layout');
+
+ 	});
  
 })  
  
